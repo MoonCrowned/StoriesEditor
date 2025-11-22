6248f54c7f5702231fd513a3f72e96598a96e5e2
MoonCrowned <loginovpb@gmail.com>
2025-11-22 20:43:49 +0300

    no secret

--- GPT_Image_API/config.js
+++ GPT_Image_API/config.js
@@ -1,5 +1,5 @@
 export const GPT_IMAGE_CONFIG = {
-    apiKey: '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',
+    apiKey: '',
     apiBaseUrl: 'https://api.openai.com/v1',
     model: 'gpt-image-1',
     size: '1024x1024',
