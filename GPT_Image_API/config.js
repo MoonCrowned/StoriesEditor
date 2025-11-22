cd54bd35c966783eea9f737b80dedab7e0492793
MoonCrowned <loginovpb@gmail.com>
2025-11-22 20:44:56 +0300

    no gpt image

--- GPT_Image_API/config.js
+++
@@ -1,8 +0,0 @@
-export const GPT_IMAGE_CONFIG = {
-    apiKey: '',
-    apiBaseUrl: 'https://api.openai.com/v1',
-    model: 'gpt-image-1',
-    size: '1024x1024',
-    organization: '',
-    project: ''
-};
